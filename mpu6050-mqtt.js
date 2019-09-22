13badc64a4ea7b039f11bfca9fd0df34d6802b01
Matthew Piercey <piercey.matthew@gmail.com>
2019-09-21 20:55:48 -0400

    Update mpu6050-mqtt.js

--- mpu6050-mqtt.js
+++ mpu6050-mqtt.js
@@ -1,24 +1,33 @@
 /*
-An implementation of Cylon.js to publish MPU6050 sensor module readings
-to an MQTT broker running on localhost
+  An implementation of Cylon.js to publish MPU6050 sensor module readings
+  to an MQTT broker running on localhost
 
-Part of CodeRcar Utils - https://github.com/botinaboxer/codercar-utils
-More info: https://www.npmjs.com/package/cylon
+  Part of CodeRcar Utils - https://github.com/botinaboxer/codercar-utils
 
-(It's especially useful for use with Node-RED!)
+  (It's especially useful for use with Node-RED!)
+  It's probably worth noting that this script requires the following Node.JS
+  modules to be installed via either NPM or Yarn (probably best to make a
+  separate folder for the script first):
 
-It's probably worth noting that this script requires the following Node.JS
-modules to be installed via either NPM or Yarn (probably best to make a
-separate folder for the script first):
+  `yarn add cylon cylon-gpio cylon-i2c cylon-mqtt cylon-raspi kalmanjs` OR
+  `npm install cylon cylon-gpio cylon-i2c cylon-mqtt cylon-raspi kalmanjs`
 
-`yarn add cylon cylon-gpio cylon-i2c cylon-mqtt cylon-raspi` OR
-`npm install cylon cylon-gpio cylon-i2c cylon-mqtt cylon-raspi`
-
-Then you just run the script like so:
-`node mpu6050-mqtt.js`
+  Then you just run the script like so:
+  `node mpu6050-mqtt.js`
 */
 
-let Cylon = require('cylon')
+const Cylon = require('cylon') // https://www.npmjs.com/package/cylon
+const KalmanFilter = require('kalmanjs') // https://github.com/infusion/Kalman.js/
+
+const accelxFilter = new KalmanFilter({R: 0.05, Q: 2})
+const accelyFilter = new KalmanFilter({R: 0.05, Q: 2})
+const accelzFilter = new KalmanFilter({R: 0.05, Q: 2})
+const gyroxFilter = new KalmanFilter({R: 0.05, Q: 2})
+const gyroyFilter = new KalmanFilter({R: 0.05, Q: 2})
+const gyrozFilter = new KalmanFilter({R: 0.05, Q: 2})
+const tempFilter = new KalmanFilter({R: 0.05, Q: 5})
+
+// ^ Using a Kalman filter to try to reduce sensor input noise
 
 Cylon.robot({
   connections: {
@@ -33,28 +42,27 @@ Cylon.robot({
   work: function(my) {
     every((1).seconds(), () => {
       my.mpu6050.getMotionAndTemp((err, data) => {
+
         console.log(data)
-        my.server.publish('accelerometer/x', data['a'][0].toString(10))
-        my.server.publish('accelerometer/y', data['a'][1].toString(10))
-        my.server.publish('accelerometer/z', data['a'][2].toString(10))
-
-        my.server.publish('gyroscope/x', data['g'][0].toString(10))
-        my.server.publish('gyroscope/y', data['g'][1].toString(10))
-        my.server.publish('gyroscope/z', data['g'][2].toString(10))
-
-        my.server.publish('temperature', data['t'].toFixed(2).toString(10))
         
-        
+        my.server.publish('accelerometer/x', accelxFilter.filter(data['a'][0]).toString(10))
+        my.server.publish('accelerometer/y', accelyFilter.filter(data['a'][0]).toString(10))
+        my.server.publish('accelerometer/z', accelzFilter.filter(data['a'][2]).toString(10))
+
+        my.server.publish('gyroscope/x', gyroxFilter.filter(data['g'][0]).toString(10))
+        my.server.publish('gyroscope/y', gyroyFilter.filter(data['g'][1]).toString(10))
+        my.server.publish('gyroscope/z', gyrozFilter.filter(data['g'][2]).toString(10))
+
+        my.server.publish('temperature', tempFilter.filter(data['t'].toFixed(2)).toString(10))
+        /*
+          ^ This part of the script should make a bit more sense if you realize
+            that the `data` object looks something like this:
+          `{ a: [ -312, 88, 16884 ],
+             g: [ -204, -32, -112 ],
+             t: 29.023529411764706
+           }`
+        */
       })
-/*
-      ^ This part of the script should make a bit more sense if you realize 
-        that the `data` object looks something like this:
-
-      `{ a: [ -312, 88, 16884 ],
-         g: [ -204, -32, -112 ],
-         t: 29.023529411764706 
-       }`
-*/
     })
   }
-}).start()
+})
